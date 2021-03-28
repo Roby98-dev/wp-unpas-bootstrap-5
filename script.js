4d13aa49487eec63148713953f3c16b47a3cb33c
Roby98-dev <bbmrobi3@gmail.com>
2021-03-29 05:11:14 +0800

    2 files modified

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbzE8PXtRHrgrS-HbOxnKQivQTVVZY0aBAJCUf3uLUnYNeW4LDGcBCgVa2KYNfWCUql1/exec";
 const form = document.forms["submit-to-google-sheet"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
